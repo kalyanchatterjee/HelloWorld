e91a1b8baaf58950078b4a444a35bf98a02b4c5a
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2021-04-14 16:53:38 -0400

    all code from work laptop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { ObservablesComponent } from './components/observables/observables.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorsComponent,
     ObservablesComponent,
-    PostsComponent
+    PostsComponent,
+    DirectivesComponent,
+    ContactFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
