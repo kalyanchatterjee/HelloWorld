1d768bdb1d112a57be631589856b2ad52f013d63
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2021-04-15 16:45:24 -0400

    End of work day 15 Apr 2021

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'checkout',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  cardForm = new FormGroup({
+    name: new FormControl(''),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
